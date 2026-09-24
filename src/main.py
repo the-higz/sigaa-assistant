e8cfbf0cfa2aa8bf48f2e8e27dbef91b89040f2d
the-higz <guilhermehenrique.sousa@discente.ufcat.edu.br>
2026-09-24 14:00:32 -0300

    feat: detect successful SIGAA login

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 
 driver = webdriver.Chrome()
 driver.get("https://sigaa.sistemas.ufcat.edu.br/sigaa/verTelaLogin.do")
 
-print("Faça login e pressione ENTER quando terminar")
-
-input()
+wait = WebDriverWait(driver, 10)
+wait.until(EC.url_to_be("https://sigaa.sistemas.ufcat.edu.br/sigaa/portais/discente/discente.jsf"))
 
 driver.quit()
