0a1155b3057c97ad79d47b01bcadfdda50e4b014
the-higz <guilhermehenrique.sousa@discente.ufcat.edu.br>
2026-09-24 18:41:33 -0300

    feat: handle activity deadlines

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium import webdriver
+from datetime import datetime
 
 driver = webdriver.Chrome()
 driver.get("https://sigaa.sistemas.ufcat.edu.br/sigaa/verTelaLogin.do")
@@ -21,12 +22,25 @@ lista_atividades = []
 for atividade in atividades:
     colunas = atividade.find_elements(By.TAG_NAME, "td")
 
-    data = colunas[1].text.split
     informaçoes = colunas[2].text.split("\n")
     disciplina = informaçoes[0]
     tipo_nome = informaçoes[1].split(":")
-    tipo = tipo_nome[0]
     nome = tipo_nome[1].lstrip()
+    tipo = tipo_nome[0]
+
+    data_dias = colunas[1].text.split(" ")
+    
+    if(tipo == "Avaliação"):
+        data = data_dias[0]
+    else:
+        data = data_dias[0] + " " + data_dias[1]
+        data_convertida = datetime.strptime(data, "%d/%m/%Y %H:%M")
+
+        agora = datetime.now()
+
+        print(data_convertida)
+        print(agora)    
+        print(data_convertida > agora)
 
     lista_atividades.append({'data': data, 'disciplina': disciplina, "tipo": tipo, "nome": nome})
 
