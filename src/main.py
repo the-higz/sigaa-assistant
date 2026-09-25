b9c108389fabd46f387a237e3de5adc56474a0bd
the-higz <guilhermehenrique.sousa@discente.ufcat.edu.br>
2026-09-25 12:55:22 -0300

    refactor: separate activity processing

--- src/main.py
+++ src/main.py
@@ -1,22 +1,14 @@
-from datetime import datetime
+from processor import processar_atividades
 from scraper import coletar_dados
 from output import gerar_arquivo
 
 dados = coletar_dados()
+
+dados = processar_atividades(dados)
+
 atividades_ativas = []
 
-agora = datetime.now()
-
 for atividade in dados:
-
-    data_convertida = datetime.strptime(
-        atividade["data"],
-        "%d/%m/%Y %H:%M"
-    )
-
-    situacao = data_convertida > agora
-
-    atividade["ativa"] = situacao
 
     if atividade["ativa"]:
         atividades_ativas.append(atividade)
---
+++ src/processor.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+def processar_atividades(atividades):
+
+    agora = datetime.now()
+
+    for atividade in atividades:
+
+        data_convertida = datetime.strptime(
+            atividade["data"],
+            "%d/%m/%Y %H:%M"
+        )
+
+        situacao = data_convertida > agora
+
+        atividade["ativa"] = situacao
+
+    return atividades
